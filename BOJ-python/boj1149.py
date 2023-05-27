95efdd74db134d6cdc184cf4d2710cefc6cbf7b5
lhj8390 <lhj8390@naver.com>
2023-05-27 22:55:41 +0900

    boj:1149

--- BOJ-python/boj1149.py
+++ BOJ-python/boj1149.py
@@ -1,13 +1,19 @@
-from itertools import permutations
-
 if __name__ == '__main__':
     n = int(input())
-    arr = [[] * n] * n
+    arr = [[0] * n] * n
     for i in range(n):
         r, g, b = map(int, input().split())
         arr[i] = [r, g, b]
 
-    print(arr)
-    per = list(permutations([arr[i] for i in range(n)], n))
-    print(per)
+    # depth 1 : 26(r), 40(g), 83(b)
+    # depth 2 : r 값, depth 1의 (b), depth 1의 (g)
+    # depth n : r[n] 값, depth n-1의 (b), depth n-1의 (g)
+    for i in range(1, len(arr)):
+        # r을 선택할 경우
+        arr[i][0] = min(arr[i - 1][1], arr[i - 1][2]) + arr[i][0]
+        # g를 선택할 경우
+        arr[i][1] = min(arr[i - 1][0], arr[i - 1][2]) + arr[i][1]
+        # b를 선택할 경우
+        arr[i][2] = min(arr[i - 1][0], arr[i - 1][1]) + arr[i][2]
 
+    print(min(arr[n - 1][0], arr[n - 1][1], arr[n - 1][2]))
