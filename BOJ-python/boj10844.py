e3c4f47d0fbc190b5fa675671a1e445fe4b1e1aa
lhj8390 <lhj8390@naver.com>
2023-06-01 23:24:50 +0900

    boj:10844 설명 추가

--- BOJ-python/boj10844.py
+++ BOJ-python/boj10844.py
@@ -1,15 +1,22 @@
 if __name__ == '__main__':
     n = int(input())
-    arr = [[0 for i in range(10)] for j in range(101)]
-    for i in range(1, 10):
+    # 0 ~ 10 까지 나열 -> 자릿수가 n 일 때, 서로 1 차이가 나는지 체크 하는 로직
+
+    # ex) n = 2 일 때,
+    # 0 1 2 3 4 5 6 7 8 9 (첫 번째 자리)
+    # 0 1 2 3 4 5 6 7 8 9 (두 번째 자리)
+    # 서로 비교
+    arr = [[0 for i in range(10)] for j in range(n + 1)]
+
+    for i in range(1, 10):  # n = 1 일 때
         arr[1][i] = 1
     for i in range(2, n + 1):
         for j in range(10):
-            if j == 0:
+            if j == 0:  # 0 일 때, 1만 계단 수
                 arr[i][j] = arr[i - 1][1]
-            elif j == 9:
+            elif j == 9:  # 9일 때, 8만 계단 수
                 arr[i][j] = arr[i - 1][8]
-            else:
+            else:  # 나머지 : +1, -1 계단 수
                 arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j + 1]
     print(sum(arr[n]) % 1000000000)
 
