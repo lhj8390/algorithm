6eca87c802fd3c92308924ce02199095a508cf79
lhj8390 <lhj8390@naver.com>
2023-06-17 21:04:59 +0900

    boj:24313

---
+++ BOJ-python/boj24313.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a, b = map(int, input().split())
+    c = int(input())
+    n = int(input())
+
+    if a * n + b <= c * n and a <= c:
+        print(1)
+    else:
+        print(0)
+
