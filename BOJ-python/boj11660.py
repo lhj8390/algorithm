54c710be606fdf4277cba77e2ff0ba3e49b3d806
lhj8390 <lhj8390@naver.com>
2023-07-02 13:13:12 +0900

    boj:11660 시간 초과

--- BOJ-python/boj11660.py
+++ BOJ-python/boj11660.py
@@ -1,17 +1,13 @@
 if __name__ == '__main__':
     n, m = map(int, input().split())
     data = [[0] * (n + 1)] * (n + 1)
-    result = 0
     for i in range(n):
         data[i] = list(map(int, input().split()))
 
-    x1, y1, x2, y2 = map(int, input().split())
-    for x in range(x1 - 1, x2):
-        if x == (x1 - 1):
-            result += sum(data[x][y1 - 1:])
-        elif x == x2:
-            result += sum(data[x][:y2])
-        else:
-            result += sum(data[x])
+    for _ in range(m):
+        result = 0
+        x1, y1, x2, y2 = map(int, input().split())
+        for x in range(x1 - 1, x2):
+            result += sum(data[x][y1 - 1: y2])
 
-    print(result)
+        print(result)
