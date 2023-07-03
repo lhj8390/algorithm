b375aed24c661ddfc2b72cc55e6c0412679e567d
lhj8390 <lhj8390@naver.com>
2023-07-03 23:25:15 +0900

    boj:11660

--- BOJ-python/boj11660.py
+++ BOJ-python/boj11660.py
@@ -1,13 +1,20 @@
+import sys
+
 if __name__ == '__main__':
+    input = sys.stdin.readline
+
     n, m = map(int, input().split())
     data = [[0] * (n + 1)] * (n + 1)
     for i in range(n):
         data[i] = list(map(int, input().split()))
 
-    for _ in range(m):
-        result = 0
-        x1, y1, x2, y2 = map(int, input().split())
-        for x in range(x1 - 1, x2):
-            result += sum(data[x][y1 - 1: y2])
+    result = [[0] * (n + 1) for _ in range(n + 1)]
 
-        print(result)
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            result[i][j] = result[i - 1][j] + result[i][j - 1] - result[i - 1][j - 1] + data[i - 1][j - 1]
+
+    for _ in range(m):
+        x1, y1, x2, y2 = map(int, input().split())
+
+        print(result[x2][y2] - result[x1 - 1][y2] - result[x2][y1 - 1] + result[x1 - 1][y1 - 1])
