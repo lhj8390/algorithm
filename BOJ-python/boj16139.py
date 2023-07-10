2bf338e66a91e404d2d1653aa28e41236773d214
lhj8390 <lhj8390@naver.com>
2023-07-10 23:24:17 +0900

    boj:16139 부분합으로 변경

--- BOJ-python/boj16139.py
+++ BOJ-python/boj16139.py
@@ -1,8 +1,21 @@
+import sys
+import string
+
 if __name__ == '__main__':
+    input = sys.stdin.readline
     s = list(input())
     q = int(input())
 
+    result = {}
+    for k in string.ascii_lowercase:
+        result[ord(k)] = [0]
+        count = 0
+        for i in range(len(s)):
+            if s[i] == k:
+                count += 1
+            result[ord(k)].append(count)
+
     for i in range(q):
         a, l, r = map(str, input().split())
-        s1 = s[int(l):int(r) + 1]
-        print(s1.count(a))
+        l, r = int(l), int(r)
+        print(result[ord(a)][r + 1] - result[ord(a)][l])
