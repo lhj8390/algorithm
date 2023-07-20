c93e6e5cb73bc983da18f25506eb0b1baa6f1eca
lhj8390 <lhj8390@naver.com>
2023-07-20 22:53:52 +0900

    boj 1931 설명추가

--- BOJ-python/boj1931.py
+++ BOJ-python/boj1931.py
@@ -4,11 +4,13 @@ if __name__ == '__main__':
     for _ in range(n):
         arr.append(list(map(int, input().split())))
 
+    # 끝나는 시간이 제일 적은 순
     arr.sort(key=lambda x: (x[1], x[0]))
 
     end = 0
     cnt = 0
     for start, finish in arr:
+        # 끝나는 시간이 다음 시작 시간 보다 작을 경우
         if end <= start:
             cnt += 1
             end = finish
