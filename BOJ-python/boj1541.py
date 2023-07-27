a2ded323a2146edb8d8867a295843b9d9d6f423a
lhj8390 <lhj8390@naver.com>
2023-07-27 22:57:30 +0900

    boj 1541

--- BOJ-python/boj1541.py
+++ BOJ-python/boj1541.py
@@ -1,6 +1,13 @@
-import re
-
 if __name__ == '__main__':
     data = input()
-    data = re.split('\\+-', data)
-    print(data)
+    data = data.split('-')
+
+    sums = 0
+    for d in data[0].split('+'):
+        sums += int(d)
+
+    for d in data[1:]:
+        for d2 in d.split('+'):
+            sums -= int(d2)
+
+    print(sums)
