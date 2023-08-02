b4dacba4a5609c5419f71a7472715439cbfe28dc
lhj8390 <lhj8390@naver.com>
2023-08-02 22:57:02 +0900

    boj 13305

--- BOJ-python/boj13305.py
+++ BOJ-python/boj13305.py
@@ -1,4 +1,15 @@
 if __name__ == '__main__':
     n = int(input())
-    dist = map(int, input())
-    prices = list(map(int, input()))
+    dist = list(map(int, input().split()))
+    prices = list(map(int, input().split()))
+
+    min_price = min(prices[:len(prices) - 1])
+    price = 0
+    for idx, d in enumerate(dist):
+        if prices[idx] == min_price:
+            price += sum(dist[idx:]) * prices[idx]
+            break
+        else:
+            price += d * prices[idx]
+
+    print(price)
