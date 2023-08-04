4005b4f4d9a995f2f9614f1211b567a12c56785f
lhj8390 <lhj8390@naver.com>
2023-08-04 21:50:30 +0900

    boj 13305 score 100

--- BOJ-python/boj13305.py
+++ BOJ-python/boj13305.py
@@ -1,15 +1,16 @@
+import sys
+
 if __name__ == '__main__':
+    input = sys.stdin.readline
     n = int(input())
     dist = list(map(int, input().split()))
     prices = list(map(int, input().split()))
 
-    min_price = min(prices[:len(prices) - 1])
+    min_price = sys.maxsize
     price = 0
-    for idx, d in enumerate(dist):
-        if prices[idx] == min_price:
-            price += sum(dist[idx:]) * prices[idx]
-            break
-        else:
-            price += d * prices[idx]
+    for i in range(n - 1):
+        if prices[i] < min_price:
+            min_price = prices[i]
+        price += min_price * dist[i]
 
     print(price)
