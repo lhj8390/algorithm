ebe18a3d0f50b66b9bf1da02cd9cb39a2f3ce473
lhj8390 <lhj8390@naver.com>
2023-08-05 22:58:14 +0900

    boj:13305 설명추가

--- BOJ-python/boj13305.py
+++ BOJ-python/boj13305.py
@@ -9,8 +9,11 @@ if __name__ == '__main__':
     min_price = sys.maxsize
     price = 0
     for i in range(n - 1):
+        # 거리를 이동하면서 최솟값 구하기
         if prices[i] < min_price:
+            # 최솟값을 찾았을 경우 갱신
             min_price = prices[i]
+        # 최솟값에 거리 값을 곱하는게 제일 최소의 비용
         price += min_price * dist[i]
 
     print(price)
