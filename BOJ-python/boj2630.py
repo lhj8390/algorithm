9179794f8fa9b35f393a8479fc78428b74e93469
lhj8390 <lhj8390@naver.com>
2023-08-07 23:14:56 +0900

    boj:2360 unsolved

--- BOJ-python/boj2630.py
+++ BOJ-python/boj2630.py
@@ -5,3 +5,14 @@ if __name__ == '__main__':
         sub = list(map(int, input().split()))
         arr.append(sub)
 
+    white = 0
+    blue = 0
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] == 1:
+                white += 1
+            else:
+                blue += 1
+
+    print(white)
+    print(blue)
