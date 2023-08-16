9d87612f0c7f29b22c61f6e4d283a594c1ccc058
lhj8390 <lhj8390@naver.com>
2023-08-16 23:53:32 +0900

    boj 1654 unsolved

--- BOJ-python/boj1654.py
+++ BOJ-python/boj1654.py
@@ -4,9 +4,15 @@ if __name__ == '__main__':
     for _ in range(k):
         line.append(int(input()))
 
-    # 최대 랜선의 길이를 구하려면
-    # 구할 수 있는 N 개의 랜선에 대한 최솟값 구하기
+    line.sort()
+    div_num = 2
+    # 개수가 n이 될 때까지 반복
+    while True:
+        tmp = 0
+        for li in line:
+            tmp += li // div_num
 
-    min_num = sum(line) // n
-
-
+        if tmp == n:
+            break
+        else:
+            div_num += 1
