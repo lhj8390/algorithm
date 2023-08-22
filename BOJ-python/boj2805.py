e8bde767bc5353cd7cb144298d475df7bbf87c54
lhj8390 <lhj8390@naver.com>
2023-08-22 23:12:16 +0900

    boj:2805 설명 추가

--- BOJ-python/boj2805.py
+++ BOJ-python/boj2805.py
@@ -4,16 +4,17 @@ if __name__ == '__main__':
 
     min_height, max_height = 1, max(heights)
 
+    # 이분탐색 수행
     while min_height <= max_height:
-        mid = (min_height + max_height) // 2
+        mid = (min_height + max_height) // 2  # 중간 길이 찾기
         height = 0
         for h in heights:
             if h > mid:
-                height += (h - mid)
+                height += (h - mid)  # 중간 길이로 나무를 자른 뒤 높이 구하기
 
-        if height >= m:
+        if height >= m:  # M미터 이상일 경우 최소 높이 증가
             min_height = mid + 1
-        else:
+        else:  # M미터 미만일 경우 최대 높이 감소
             max_height = mid - 1
 
     print(max_height)
